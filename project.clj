b2eff9aad45b0534e8a65acf8c663c3f79c15196
Eric Lavigne <lavigne.eric@gmail.com>
2010-08-09 22:27:02 -0400

    Added dependencies: Scriptjure, jQuery, and FleetDB.

--- project.clj
+++ project.clj
@@ -5,5 +5,12 @@
 		 [org.clojure/clojure-contrib "1.1.0"]
 		 [compojure "0.4.0"]
 		 [ring/ring-jetty-adapter "0.2.3"]
-		 [hiccup "0.2.6"]]
+		 [hiccup "0.2.6"]
+		 [scriptjure "0.1.11"]
+		 [fleetdb "0.2.0"]]
   :dev-dependencies [[leiningen-run "0.3"]])
+
+; Note: the project.clj file for FleetDB server is instructive
+; with respect to Leiningen options and available libraries.
+; http://github.com/mmcgrana/fleetdb/blob/master/project.clj
+
