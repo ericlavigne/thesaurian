d65d2cc6bf68d292b6264fca61bec3670ec154c0
Eric Lavigne <lavigne.eric@gmail.com>
2010-11-27 21:21:54 -0500

    Upgraded library versions.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject thesaurian "0.1" 
   :description "Web application for creating controlled vocabularies." 
-  :main thesaurian.main
-  :dependencies [[org.clojure/clojure "1.1.0"] 
-		 [org.clojure/clojure-contrib "1.1.0"]
-		 [compojure "0.4.0"]
-		 [ring/ring-jetty-adapter "0.2.3"]
-		 [hiccup "0.2.6"]
-		 [scriptjure "0.1.11"]
+  :dependencies [[org.clojure/clojure "1.2.0"] 
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.5.3"]
+		 [ring/ring-jetty-adapter "0.3.4"]
+		 [hiccup "0.3.1"]
+		 [scriptjure "0.1.20"]
 		 [fleetdb "0.2.0"]]
-  :dev-dependencies [[leiningen-run "0.3"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [lein-run "1.0.0"]])
 
 ; Note: the project.clj file for FleetDB server is instructive
 ; with respect to Leiningen options and available libraries.
--- src/thesaurian/main.clj
+++ src/thesaurian/main.clj
@@ -2,18 +2,15 @@
   (:use compojure.core
 	ring.adapter.jetty)
   (:require [compojure.route :as route]
-	    [thesaurian.view :as view])
-  (:gen-class))
+	    [thesaurian.view :as view]))
 
 (defroutes main-routes
   (route/files "/" {:root "static"})
   (GET "/" [] (view/index))
   (route/not-found "That page does not exist."))
 
-(defn -main [& args]
+(defn main [& args]
   (let [port (if (empty? args)
 	       8080
 	       (Integer/parseInt (first args)))]
     (run-jetty main-routes {:port port})))
-
-
